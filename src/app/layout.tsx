652160f64ba44d3169aa3de276e9e3486307fd41
Edward-126 <edwardhyde20126@gmail.com>
2024-11-06 17:30:38 +0530

    style: basic styles added, fonts added (inter/bebas_neue), button styles modified.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-const inter = Inter({ subsets: ["latin"] });
+import { INTER } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "1705 Studio Assignment",
   description:
-    "Project submission for 1705 Studio's assignment by Thilina Rathnayaka, implementing design and functionality as specified. Developed to meet the studio's standards with attention to detail and quality.",
+    "Project submission for 1705 Studio's assignment by Thilina Rathnayaka, implementing design and functionality as specified.",
 };
 
 export default function RootLayout({
@@ -19,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn("relative h-full font-sans antialiased", inter.className)}
+        className={cn("relative h-full font-sans antialiased", INTER.className)}
       >
         <main className="relative flex min-h-screen flex-col">
           <div className="flex-1 flex-grow">{children}</div>
---
+++ src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
+
+export const INTER = Inter({ subsets: ["latin"] });
+export const BEBAS_NEUE = Bebas_Neue({
+  weight: ["400"],
+  subsets: ["latin"],
+});
